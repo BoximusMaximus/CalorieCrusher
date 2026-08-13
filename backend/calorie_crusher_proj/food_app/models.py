cad5e8959a88b50ee90b2c270de7091ac9922486
BoximusMaximus <chris83337@gmail.com>
2026-08-13 13:34:09 +0000

    added types of food for food model

--- backend/calorie_crusher_proj/food_app/models.py
+++ backend/calorie_crusher_proj/food_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Food (models.Model):
+    TYPES_OF_FOOD = {
+        "VG" : "Vegetable",
+        "FT" : "Fruit",
+        "GR" : "Grain",
+        "DA" : "Dairy"
+    }
