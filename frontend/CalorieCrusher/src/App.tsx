aaf4d67de17978037427b5dd516a7501cd2b71c6
BoximusMaximus <chris83337@gmail.com>
2026-08-18 19:36:00 +0000

    created frontend Vite app to begin interacting with the db

---
+++ frontend/CalorieCrusher/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Outlet } from "react-router"
+import LinkBar from './components/LinkBar'
+
+function App() {
+  return (
+    <>
+      {/* <LinkBar/> */}
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
